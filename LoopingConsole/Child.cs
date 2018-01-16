a811fa64b0914d071d9cb8936a7eaa0ea7b46935
Nathan Gonzalez <nathan@copypastedeveloper.com>
2018-01-15 20:50:15 -0600

    created some children

---
+++ LoopingConsole/Child.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopingConsole
+{
+    class Child
+    {
+        public string Name { get; set; }
+        public bool Sick { get; set; }
+
+        public string ForgeASickNote()
+        {
+            return Sick
+                ? $"Please excuse {Name} from class today because they are very sick."
+                : $"{Name}, you still have to go tgo school, quit faking.  Love Mom";
+        }
+    }
+}
--- LoopingConsole/Program.cs
+++ LoopingConsole/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LoopingConsole
 {
@@ -10,6 +8,43 @@ namespace LoopingConsole
     {
         static void Main(string[] args)
         {
+            var children = new List<Child>
+            {
+                new Child {Name = "Suzie", Sick = false},
+                new Child {Name = "Ralph", Sick = true}
+            };
+
+
+            //while (true)
+            //{
+            //    //do something awesome
+            //}
+
+            //for (var i = 0; i < children.Count - 1; i++)
+            //{
+            //    var child = children[i];
+            //    Console.WriteLine($"{child.Name} is {(child.Sick ? "sick" : "healthy")}");
+            //    Console.WriteLine(child.ForgeASickNote());
+            //}
+
+            foreach (var child in children)
+            {
+                //if (child.Name == "Suzie")
+                //{
+                //    continue;
+                //}
+                //if (child.Name == "Suzie")
+                //{
+                //    break;
+                //}
+
+                Console.WriteLine($"{child.Name} is {(child.Sick ? "sick" : "healthy")}");
+                Console.WriteLine(child.ForgeASickNote());
+            }
+
+            Console.WriteLine("This is our second time talking aboout the same thing.  #Nojudgement.");
+
+            var lineIRead = Console.ReadLine();
         }
     }
 }
